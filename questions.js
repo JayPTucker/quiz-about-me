ceb389f86b1841972d5f36a57da29d193043b8c5
JayPTucker <jayptuckerimportant@gmail.com>
2020-02-27 15:45:05 -0600

    Added JavaScript Code for the Questions.  Added a Questions.js file

---
+++ questions.js
@@ -0,0 +1,15 @@
+var questions = [{
+    "question": "What's my Name?",
+    "option1": "Abacus",
+    "option2": "Analytical Engine",
+    "option3": "Calculator",
+    "option4": "Processor",
+    "answer": "2"
+}, {
+    "question": "What's my Last Name?",
+    "option1": "Abacus",
+    "option2": "Analytical Engine",
+    "option3": "Calculator",
+    "option4": "Processor",
+    "answer": "2"
+}]
