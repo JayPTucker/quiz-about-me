15e18e233c32972de19b7d53a082dd648ec3dd8e
JayPTucker <jayptuckerimportant@gmail.com>
2020-03-01 17:43:59 -0600

    Redesigned the Quiz portion with better positioned items.

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,10 +1,10 @@
 var questions = [{
-    "question": "What's my Name?",
-    "option1": "Abacus",
-    "option2": "Analytical Engine",
-    "option3": "Calculator",
-    "option4": "Processor",
-    "answer": "2"
+    "question": "What's my FIRST Name?",
+    "option1": "Jay",
+    "option2": "John Paul",
+    "option3": "Jay Paul",
+    "option4": "xXxPoisonMaster493xXx",
+    "answer": "3"
 }, {
     "question": "What's my Last Name?",
     "option1": "Abacus",
--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ var totQuestions = questions.length;
 
 // MAIN MENU END
 
-var container = document.getElementById('quizContainer');
+var quizBox = document.getElementById('quiz');
 var questionEl = document.getElementById('question');
 var opt1 = document.getElementById('opt1');
 var opt2 = document.getElementById('opt2');
@@ -15,41 +15,72 @@ var opt4 = document.getElementById('opt4');
 var answers = document.getElementById('answers');
 var nextButton = document.getElementById('nextButton');
 var resultCont = document.getElementById('result');
+var timerScore = document.getElementById('timerScore');
+var leaderboard = document.getElementById('leaderboard');
+var title = document.getElementById('title');
 
 // QUIZ TIMER START
 var sec = 60;
 
 function timer(){
     var timer = setInterval(function(){
-        
-        document.getElementById('timer').innerText = "Time Left: " + sec;
+        document.getElementById('timer').innerText = "Time Remaining:  " + sec;
         sec--;
-        if (sec < 0) {
-            alert("Time's up!")
+        // 0 SECONDS LEFT ACTIONS:
+        if (sec < -1) {
+            alert("Time's up!  Let's Check your Score!");
             clearInterval(timer);
         }
+
+        // IF ALL QUESTIONS ARE COMPLETED:
         if(currentQuestion == totQuestions){
             clearInterval(timer);
+            leaderboardScore();
+            clearInterval(timer);
+        }
+
+        // LESS THAN 15 SECONDS WARNING:
+
+        if(sec < 15) {
+            document.getElementById('timer').style.color = "red";
+        }
+        else {
+            document.getElementById('timer').style.color = "white";
         }
     }, 1000);
 }
 
 timer();
+
+
+function leaderboardScore() {
+    // var leaderboardName = prompt("You finished!  What's your name?");
+    localStorage.setItem("score", score);
+
+    // localStorage.setItem("date", Date());
+    // localStorage.setItem("name", leaderboardName);
+
+
+    // quiz.innerHTML = "";
+    // quiz.innerHTML = "<p class='LBScore'>Your Score: " + localStorage.getItem("score") + "</p>"
+
+}
+
 // QUIZ TIMER END
 
 function loadQuestion (questionIndex) {
     var q = questions[questionIndex];
-    questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
-    opt1.textContent = q.option1;
-    opt2.textContent = q.option2;
-    opt3.textContent = q.option3;
-    opt4.textContent = q.option4;
+    questionEl.textContent = (questionIndex + 1) + '.) ' + q.question;
+    opt1.textContent = "A.  " + q.option1;
+    opt2.textContent = "B.  " + q.option2;
+    opt3.textContent = "C.  " + q.option3;
+    opt4.textContent = "D.  " + q.option4;
 };
 
 function loadNextQuestion () {
     var selectedOption = document.querySelector('input[type=radio]:checked');
     if(!selectedOption){
-        alert('Please select your answer!');
+        alert('No Skipping!  You must select at least ONE answer below before continuing.');
         return;
     }
 
@@ -57,28 +88,24 @@ function loadNextQuestion () {
     var answer = selectedOption.value;
     if(questions[currentQuestion].answer == answer) {
         score += 10;
-        document.getElementById('timerScore').innerHTML = "<p>Correct!  +10 to the Timer!</p>"
+        sec += 10;
+        timerScore.style.color = "lightgreen";
+        timerScore.innerHTML = "<p>Correct!  +10 to the Timer!</p>";
     } else {
         score -= 15;
-        document.getElementById('timerScore').innerHTML = "<p>Incorrect!  -15 from the Timer!</p>"
-    }
-
-    // TIMER CALCULATOR
-    if(questions[currentQuestion].answer == answer) {
-        sec += 10;
-    } else {
         sec -= 15;
+        timerScore.style.color = "red";
+        timerScore.innerHTML = "<p>Incorrect!  -15 from the Timer!</p>";
     }
+    
 
+// NEXT QUESTION
     selectedOption.checked = false;
     currentQuestion++;
     if(currentQuestion == totQuestions - 1){
         nextButton.textContent = 'Finish';
     }
-    if(currentQuestion == totQuestions){
-        console.log('Your score: ' + score)
-        return;
-    }
+
     loadQuestion(currentQuestion);
 }
 
